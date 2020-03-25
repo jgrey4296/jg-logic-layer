7f10eff0ca3dbd3c6712a8782dc3fc1d3c0fd0ee
John Grey <johngrey4296@gmail.com>
2020-03-25 00:49:50 +0000

    add modes to auto-mode-alist

--- packages.el
+++ packages.el
@@ -14,6 +14,7 @@
     (org-babel-do-load-languages
      'org-babel-load-languages '((ccalc . t)))
     (push '("ccalc" . prolog) org-src-lang-modes)
+    (add-to-list 'auto-mode-alist '("\\.ccalc$" . prolog-mode))
     )
   )
 (defun jg-logic-layer/init-ob-clingo ()
@@ -22,6 +23,7 @@
     (org-babel-do-load-languages
      'org-babel-load-languages '((clingo . t)))
     (push '("clingo" . prolog) org-src-lang-modes)
+    (add-to-list 'auto-mode-alist '("\\.lp$" . prolog-mode))
     )
 
   )
@@ -31,6 +33,7 @@
     (org-babel-do-load-languages
      'org-babel-load-languages '((prolog . t)))
     (setq org-babel-prolog-command "swipl")
+    (add-to-list 'auto-mode-alist '("\\.lp$" . prolog-mode))
     )
   )
 
